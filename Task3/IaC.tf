e80103dfc7dee159678d68b0e39a2c627317445d
makhmudjon.rakhmatullaev <makhmudjon.rakhmatullaev@gmail.com>
2022-02-25 12:22:27 +0500

    Task3 commit

--- Task3/IaC.tf
+++ Task3/IaC.tf
@@ -1,18 +1,13 @@
 provider "aws" {}
 
-variable "centos-ami" {
-  default = "ami-0005dd1bdf84d89d0"
-}
 
-variable "ubuntu-ami" {
-  default = "ami-0d527b8c289b4af7f"
-}
+### EC2 INSTANCES ###
 
 # Centos
 resource "aws_instance" "instance_1" {
   ami                    = var.centos-ami
   instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.SG_instance_1.id]
+  vpc_security_group_ids = ["${aws_security_group.SG_instance_1.id}"]
   user_data              = file("./user_data_1.sh")
   tags = {
     Name = "instance-1.tf"
@@ -23,16 +18,84 @@ resource "aws_instance" "instance_1" {
 resource "aws_instance" "instance_2" {
   ami                    = var.ubuntu-ami
   instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.SG_instance_2.id]
+  vpc_security_group_ids = ["${aws_security_group.SG_instance_2.id}"]
   user_data              = file("./user_data_2.sh")
   tags = {
     Name = "instance-2.tf"
   }
-
 }
-# output "ip_internal" {
-#   value     = "$${curl http://169.254.169.254/latest/meta-data/local-ipv4}"
-#   sensitive = true
+
+### VPC ###
+
+resource "aws_vpc" "prod-vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = "true"
+  enable_dns_hostnames = "true"
+  enable_classiclink   = "false"
+  instance_tenancy     = "default"
+  tags = {
+    Name = "prod-vpc"
+  }
+}
+
+### SUBNETS ###
+
+# PRIVATE SUBNET
+
+resource "aws_subnet" "prod-subnet-private" {
+  vpc_id                  = aws_vpc.prod-vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = "false"
+  availability_zone       = "eu-central-1a"
+  tags = {
+    Name = "prod-subnet-private"
+  }
+}
+
+# PUBLIC SUBNET
+
+resource "aws_subnet" "prod-subnet-public" {
+  vpc_id                  = aws_vpc.prod-vpc.id
+  cidr_block              = "10.0.2.0/24"
+  map_public_ip_on_launch = "true" // if false, makes subnet private
+  availability_zone       = "eu-central-1b"
+  tags = {
+    Name = "prod-subnet-public"
+  }
+}
+
+
+### INTERNET GATEWAY ###
+resource "aws_internet_gateway" "prod-igw" {
+  vpc_id = aws_vpc.prod-vpc.id
+  tags = {
+    Name = "prod-igw"
+  }
+}
+
+### ROUTE TABLES ###
+
+#Public
+resource "aws_route_table" "prod-public-rtb" {
+  vpc_id = aws_vpc.prod-vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.prod-igw.id
+  }
+  tags = {
+    Name = "prod-public-rtb"
+  }
+}
+
+# ASSOCIATE RTB TO SUBNET
+
+# Private
+resource "aws_route_table_association" "prod-rtb-private-subnet" {
+  subnet_id      = aws_subnet.prod-subnet-public.id
+  route_table_id = aws_route_table.prod-public-rtb.id
+}
+
+### SECURITY GROUPS ###
 
 # SG for Centos
 resource "aws_security_group" "SG_instance_1" {
--- Task3/user_data_2.sh
+++ Task3/user_data_2.sh
@@ -1,11 +1,33 @@
 #!/bin/bash
+# SSH key add
+echo 'PubkeyAuthentication yes' >> /etc/ssh/sshd_config
+echo 'AuthorizedKeysFile      .ssh/authorized_keys' >> /etc/ssh/sshd_config
+systemctl reload sshd
 
 # Nginx installation
 sudo apt update -y && sudo apt install nginx -y && -y sudo ufw allow 'Nginx HTTP' && systemctl start nginx && systemctl enable nginx
 os=`cat /etc/os-release`
 mem=`free -h`
-echo "
-<!DOCTYPE html><html><head><title>Task3</title></head><body><h1><strong>Hello World!</strong></h1><div><p>$os</p><br><h4>Memory</h4><p>$mem</p></div></body></html>" > /var/www/html/index.html
-
 # Docker installation
+sudo apt-get update
+
+sudo apt-get install \
+ca-certificates \
+curl \
+gnupg \
+lsb-release
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
+"deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y && systemctl start docker && systemctl enable docker
+
+docker=`docker --version`
+
+echo "
+# Edit Nginx default web page
+<!DOCTYPE html><html><head><title>Task3</title></head><body><h1><strong>Hello World!</strong></h1><div><p>$os</p><br><h4>Memory</h4><p>$mem</p><br><p>$docker is Installed</p></div></body></html>" > /var/www/html/index.html
