a5d3fa856ddb8d36fc21026a83196d539ae3969c
makhmudjon.rakhmatullaev <makhmudjon.rakhmatullaev@gmail.com>
2022-02-28 10:12:09 +0500

    Task3 commit

--- Task3/IaC.tf
+++ Task3/IaC.tf
@@ -1,5 +1,29 @@
-provider "aws" {}
+provider "aws" {
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_access_key
+  region     = var.aws_region
 
+}
+
+variable "aws_region" {
+  default = "eu-central-1"
+}
+
+variable "aws_secret_access_key" {
+  default = "QXZR6dywYzR0BqbnCEgguBqXiH3fZAt1qFuJsBEi"
+}
+
+variable "aws_access_key_id" {
+  default = "AKIAQKBQSHDYEAWZKTFO"
+}
+
+variable "centos-ami" {
+  default = "ami-0005dd1bdf84d89d0"
+}
+
+variable "ubuntu-ami" {
+  default = "ami-0d527b8c289b4af7f"
+}
 
 ### EC2 INSTANCES ###
 
@@ -8,6 +32,7 @@ resource "aws_instance" "instance_1" {
   ami                    = var.centos-ami
   instance_type          = "t2.micro"
   vpc_security_group_ids = ["${aws_security_group.SG_instance_1.id}"]
+  subnet_id              = aws_subnet.prod-subnet-private.id
   user_data              = file("./user_data_1.sh")
   tags = {
     Name = "instance-1.tf"
@@ -19,6 +44,7 @@ resource "aws_instance" "instance_2" {
   ami                    = var.ubuntu-ami
   instance_type          = "t2.micro"
   vpc_security_group_ids = ["${aws_security_group.SG_instance_2.id}"]
+  subnet_id              = aws_subnet.prod-subnet-public.id
   user_data              = file("./user_data_2.sh")
   tags = {
     Name = "instance-2.tf"
@@ -89,8 +115,8 @@ resource "aws_route_table" "prod-public-rtb" {
 
 # ASSOCIATE RTB TO SUBNET
 
-# Private
-resource "aws_route_table_association" "prod-rtb-private-subnet" {
+#Public
+resource "aws_route_table_association" "prod-rtb-public-subnet" {
   subnet_id      = aws_subnet.prod-subnet-public.id
   route_table_id = aws_route_table.prod-public-rtb.id
 }
@@ -99,28 +125,74 @@ resource "aws_route_table_association" "prod-rtb-private-subnet" {
 
 # SG for Centos
 resource "aws_security_group" "SG_instance_1" {
+  vpc_id = aws_vpc.prod-vpc.id
+
   tags = {
     Name = "SG | instance_1"
   }
+
   ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["${aws_instance.instance_2.private_ip}/32"]
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["${aws_vpc.prod-vpc.cidr_block}"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["${aws_vpc.prod-vpc.cidr_block}"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${aws_vpc.prod-vpc.cidr_block}"]
+  }
+
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["${aws_vpc.prod-vpc.cidr_block}"]
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["${aws_instance.instance_2.private_ip}/32"]
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["${aws_vpc.prod-vpc.cidr_block}"]
   }
 
+  egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["${aws_vpc.prod-vpc.cidr_block}"]
+  }
 
+  egress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${aws_vpc.prod-vpc.cidr_block}"]
+  }
+
+  egress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["${aws_vpc.prod-vpc.cidr_block}"]
+  }
 }
+
 
 # SG for Ubuntu
 resource "aws_security_group" "SG_instance_2" {
+  vpc_id = aws_vpc.prod-vpc.id
+
   tags = {
     Name = "SG | instance_2"
   }
@@ -132,8 +204,8 @@ resource "aws_security_group" "SG_instance_2" {
   }
 
   ingress {
-    from_port   = 433
-    to_port     = 433
+    from_port   = 443
+    to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -142,6 +214,13 @@ resource "aws_security_group" "SG_instance_2" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
